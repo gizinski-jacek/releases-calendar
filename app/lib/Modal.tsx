d2fc7030de5d680872cd8a820e354fe5df7262e9
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-06 19:23:23 +0100

    Adding optional props to customize styling.

--- app/lib/Modal.tsx
+++ app/lib/Modal.tsx
@@ -1,23 +1,43 @@
 interface Props {
 	children: React.ReactNode;
 	closeModal: () => void;
+	fullW?: boolean;
+	fullH?: boolean;
+	bgColor?: string;
+	borderC?: string;
 }
 
-const Modal = ({ children, closeModal }: Props) => {
+const Modal = ({
+	children,
+	closeModal,
+	fullW = false,
+	fullH = false,
+	bgColor = 'bg-gray-800',
+	borderC = 'border-purple-800',
+}: Props) => {
 	return (
 		<div className='absolute top-0 left-0 right-0 bottom-0 z-10 flex justify-center items-center'>
 			<div
-				className='absolute top-0 left-0 right-0 bottom-0 bg-black/75'
-				onClick={closeModal}
-			/>
-			<div className='w-4/5 h-4/5 p-4 bg-gray-700 border-4 border-green-700 rounded-lg z-10 overflow-scroll relative'>
-				<span
-					className='text-red-500 absolute top-0 right-4 text-2xl'
+				className={`flex justify-center items-center 
+				${fullW ? 'w-full' : 'w-4/5'} ${fullH ? 'h-full' : 'h-4/5'}`}
+			>
+				<div
+					className='absolute top-0 left-0 right-0 bottom-0 bg-black/75'
 					onClick={closeModal}
+				/>
+				<div
+					className={`w-full h-full border-2 rounded-tl-lg rounded-bl-lg rounded-br-lg overflow-scroll relative shadow-md shadow-gray-500 
+					${bgColor} ${borderC}`}
 				>
-					X
-				</span>
-				<div>{children}</div>
+					<div className='flex flex-col gap-1'>{children}</div>
+				</div>
+				<div className='self-start w-0 z-10 mt-[4px] cursor-pointer'>
+					<span
+						className={`border-2 p-1 pb-1.5 border-l-0 rounded-tr-lg rounded-br-lg text-red-500 text-2xl shadow shadow-gray-500 modal-close-btn 
+						${bgColor} ${borderC}`}
+						onClick={closeModal}
+					/>
+				</div>
 			</div>
 		</div>
 	);
