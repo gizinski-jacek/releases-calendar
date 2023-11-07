9a464b8f8a887b18ce155ed945f719d969b01311
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-07 19:57:00 +0100

    Fixing changing slide with dots.
    Importing ModalImage to display in full screen full quality game images.
    Adding modal opening handler.
    Gallery will render static img with empty background if gallery is empty.
    Replacing multiple gallery.length checks with single one.

--- app/lib/wrappers/Gallery.tsx
+++ app/lib/wrappers/Gallery.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import { useState } from 'react';
+import ModalImage from '@/app/components/ModalImage';
+import bgEmpty from '/public/empty-background.png';
 
 interface Props {
 	gallery: { id: number; image: string }[];
 }
 
 const Gallery = ({ gallery }: Props) => {
-	const [slideIndex, setSlideIndex] = useState(0);
+	const [slideIndex, setSlideIndex] = useState<number>(0);
+	const [openImage, setOpenImage] = useState<string | null>(null);
 
 	const prevSlide = () => {
 		if (slideIndex <= 0) {
@@ -24,42 +27,52 @@ const Gallery = ({ gallery }: Props) => {
 		}
 	};
 
-	const changeSlideWithDots = (e: React.MouseEvent<HTMLSpanElement>) => {
-		const { id } = e.target as HTMLSpanElement;
-		setSlideIndex(Number(id));
+	const changeSlideWithDots = (value: number) => {
+		setSlideIndex(value);
 	};
 
-	return (
-		<div className='w-full h-full flex flex-col'>
-			<div
-				className='absolute top-[40%] left-0 text-xl box-border cursor-pointer select-none p-4 transition-all bg-black/75 hover:bg-black text-white rounded-tr-md rounded-br-md'
-				onClick={prevSlide}
-			>
-				&#10094;
+	const openModal = (value: string) => {
+		setOpenImage(value);
+	};
+
+	const closeModal = () => {
+		setOpenImage(null);
+	};
+
+	return gallery.length > 0 ? (
+		<>
+			<div className='w-full h-auto max-w-[600px] relative select-none'>
+				<div
+					className='absolute top-[50%] translate-y-[-50%] left-0 text-xl box-border cursor-pointer select-none p-4 transition-all bg-black/50 hover:bg-black/75 text-white rounded-tr-md rounded-br-md'
+					onClick={prevSlide}
+				>
+					&#10094;
+				</div>
+				{gallery.map((item, index) => {
+					return (
+						<div
+							key={item.id}
+							className={`${index === slideIndex ? 'block' : 'hidden'}`}
+						>
+							<Image
+								src={item.image}
+								alt='Game Image'
+								width={600}
+								height={337}
+								className='w-full h-auto'
+								onClick={() => openModal(item.image)}
+							/>
+						</div>
+					);
+				})}
+				<div
+					className='absolute top-[50%] translate-y-[-50%] right-0 text-xl box-border cursor-pointer select-none p-4 transition-all bg-black/50 hover:bg-black/75 text-white rounded-tl-md rounded-bl-md'
+					onClick={nextSlide}
+				>
+					&#10095;
+				</div>
 			</div>
-			{gallery.map((item, index) => {
-				return (
-					<div
-						key={item.id}
-						className={`${index === slideIndex ? 'block' : 'hidden'}`}
-					>
-						<Image
-							src={item.image}
-							alt='Image'
-							width={800}
-							height={450}
-							className='w-full h-full'
-						/>
-					</div>
-				);
-			})}
-			<div
-				className='absolute top-[40%] right-0 text-xl box-border cursor-pointer select-none p-4 transition-all bg-black/75 hover:bg-black text-white rounded-tl-md rounded-bl-md'
-				onClick={nextSlide}
-			>
-				&#10095;
-			</div>
-			<div className='flex justify-center items-center gap-2 m-2 my-8'>
+			<div className='flex justify-center items-center gap-2 p-1 pb-4'>
 				<div
 					className='text-xl box-border cursor-pointer select-none px-1 transition-all text-gray-300 hover:text-white'
 					onClick={prevSlide}
@@ -74,7 +87,7 @@ const Gallery = ({ gallery }: Props) => {
 								className={`inline transition-all h-4 w-4 bg-gray-500 hover:bg-gray-300 rounded-lg cursor-pointer ${
 									index === slideIndex ? 'bg-white hover:bg-white' : ''
 								}`}
-								onClick={changeSlideWithDots}
+								onClick={() => changeSlideWithDots(index)}
 							/>
 						);
 					})}
@@ -86,6 +99,20 @@ const Gallery = ({ gallery }: Props) => {
 					&#10095;
 				</div>
 			</div>
+			{openImage && (
+				<ModalImage closeModal={closeModal}>
+					<img
+						src={openImage}
+						alt='Game Image'
+						className='w-full h-auto'
+						onClick={closeModal}
+					/>
+				</ModalImage>
+			)}
+		</>
+	) : (
+		<div className='relative select-none bg-empty'>
+			<Image src={bgEmpty} alt='No game images' />
 		</div>
 	);
 };
