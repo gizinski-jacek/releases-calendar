6a6b1ca8e183cba602422cc2ba6bda5c4c884aab
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-07 19:54:40 +0100

    Adding separate Modal for displaying enlarged gallery image.

---
+++ app/components/ModalImage.tsx
@@ -0,0 +1,20 @@
+interface Props {
+	children: React.ReactNode;
+	closeModal: () => void;
+}
+
+const ModalImage = ({ children, closeModal }: Props) => {
+	return (
+		<div
+			className='w-screen h-screen absolute top-0 left-0 z-50'
+			onClick={closeModal}
+		>
+			<div className='absolute top-0 left-0 right-0 bottom-0 bg-black' />
+			<div className='absolute top-[50%] translate-y-[-50%] left-0  relative z-20'>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default ModalImage;
