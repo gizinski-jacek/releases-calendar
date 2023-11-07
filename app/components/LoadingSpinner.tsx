35c41733c2b24ad022c5c158a7215b775d3476f8
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-07 19:53:18 +0100

    Adding loading spinner component and styling

---
+++ app/components/LoadingSpinner.tsx
@@ -0,0 +1,25 @@
+import style from '../styles/LoadingSpinner.module.scss';
+
+const LoadingSpinner = () => {
+	return (
+		<div className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50'>
+			<div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50' />
+			<div className={style.spinner}>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default LoadingSpinner;
