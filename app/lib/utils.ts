ebef3bc34325c2942a3f9a5016e80d3653a992c2
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-08 22:57:29 +0100

    Refactoring grouping by date function
    to support grouping days from previous and next months.
    Adding functions calculating visible days from prev/next month in calendar.
    Cleaning up/renaming variables and functions.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -11,17 +11,21 @@ export const filterMature = (data: GameData[]): GameData[] => {
 	return filtered;
 };
 
-export const groupByDay = (
+export const groupByDate = (
 	data: GameData[],
 	year: number,
 	month: number
 ): GroupedByDay => {
-	const grouped = data.reduce((prev, curr) => {
-		prev[Number(curr.released.slice(-2)) - 1].game_releases =
-			prev[Number(curr.released.slice(-2)) - 1].game_releases || [];
-		prev[Number(curr.released.slice(-2)) - 1].game_releases.push(curr);
-		return prev;
-	}, daysInMonthToObject(year, month));
+	const grouped = [
+		...prevMonthDaysToObj(year, month),
+		...daysInMonthToObject(year, month),
+		...nextMonthDaysToObj(year, month),
+	].map((day) => {
+		return {
+			...day,
+			game_releases: data.filter((item) => item.released === day.date),
+		};
+	});
 	return grouped;
 };
 
@@ -55,14 +59,48 @@ export const getDateInString = (
 	return date;
 };
 
+export const getDaysInPrevMonth = (year: number, month: number): number => {
+	const dayCount = moment
+		.utc()
+		.year(year)
+		.month(month - 1)
+		.endOf('month')
+		.isoWeekday();
+	return dayCount;
+};
+
 export const getDaysInMonth = (year: number, month: number): number => {
-	const daysInMonth = moment
+	const dayCount = moment
 		.utc()
 		.year(year)
 		.month(month)
 		.startOf('month')
 		.daysInMonth();
-	return daysInMonth;
+	return dayCount;
+};
+
+export const getDaysInNextMonth = (year: number, month: number): number => {
+	const dayCount =
+		7 - moment.utc().year(year).month(month).endOf('month').isoWeekday();
+	return dayCount;
+};
+
+export const prevMonthDaysToObj = (
+	year: number,
+	month: number
+): GroupedByDay => {
+	const prevMonthDays = getDaysInPrevMonth(year, month);
+	const daysInPrevMonth = getDaysInMonth(year, month - 1);
+	const arr = [...Array(prevMonthDays)].map((x, i) => {
+		return {
+			date: getDateInString(year, month - 1, daysInPrevMonth - i),
+			dayOfMonth: daysInPrevMonth - i,
+			isCurrentMonth: false,
+			isCurrentDay: false,
+			game_releases: [] as GameData[],
+		};
+	});
+	return arr;
 };
 
 export const daysInMonthToObject = (
@@ -70,8 +108,9 @@ export const daysInMonthToObject = (
 	month: number
 ): GroupedByDay => {
 	const arr = [...Array(getDaysInMonth(year, month))].map((x, i) => {
+		const date = getDateInString(year, month, i + 1);
 		return {
-			date: getDateInString(year, month, i + 1),
+			date: date,
 			dayOfMonth: i + 1,
 			isCurrentMonth: true,
 			isCurrentDay: moment().date() === i + 1,
@@ -81,40 +120,12 @@ export const daysInMonthToObject = (
 	return arr;
 };
 
-export const previousMonthDaysObjToDisplay = (
+export const nextMonthDaysToObj = (
 	year: number,
 	month: number
 ): GroupedByDay => {
-	const lastMonthLastWeekDayCount = moment
-		.utc()
-		.year(year)
-		.month(month - 1)
-		.endOf('month')
-		.isoWeekday();
-	const daysInMonth = moment
-		.utc()
-		.year(year)
-		.month(month - 1)
-		.daysInMonth();
-	const arr = [...Array(lastMonthLastWeekDayCount)].map((x, i) => {
-		return {
-			date: getDateInString(year, month - 1, daysInMonth - i),
-			dayOfMonth: daysInMonth - i,
-			isCurrentMonth: false,
-			isCurrentDay: false,
-			game_releases: [] as GameData[],
-		};
-	});
-	return arr.sort((a, b) => a.dayOfMonth - b.dayOfMonth);
-};
-
-export const nextMonthDaysObjToDisplay = (
-	year: number,
-	month: number
-): GroupedByDay => {
-	const nextMonthFirstWeekDayCount =
-		7 - moment.utc().year(year).month(month).endOf('month').isoWeekday();
-	const arr = [...Array(nextMonthFirstWeekDayCount)].map((x, i) => {
+	const nextMonthDays = getDaysInNextMonth(year, month);
+	const arr = [...Array(nextMonthDays)].map((x, i) => {
 		return {
 			date: getDateInString(year, month + 1, i + 1),
 			dayOfMonth: i + 1,
