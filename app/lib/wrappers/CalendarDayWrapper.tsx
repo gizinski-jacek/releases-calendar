f164cf7241e60b99ab36359b9adeeab970459de9
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-12 23:36:30 +0100

    Adding new optional props.
    "highlight" to distinguish specific days.
    "translateOnHover" to translate specific days (mainly first and
    last) to prevent them from going offscreen.
    Cleaning up bgImages data in useEffect to correctly remove
    background images when mature filter state changes.
    Adding additional div wrapper to prevent on hover element from jittering
    back and forth if it moves from under the mouse cursor.

--- app/lib/wrappers/CalendarDayWrapper.tsx
+++ app/lib/wrappers/CalendarDayWrapper.tsx
@@ -8,9 +8,19 @@ import bgEmpty from '/public/empty-background.png';
 interface Props {
 	data: DayData;
 	styleClass?: string;
+	highlight?: boolean;
+	translateOnHover?:
+		| 'hover-translate-child-right'
+		| 'hover-translate-child-left'
+		| undefined;
 }
 
-const CalendarItemWrapper = ({ data, styleClass = '' }: Props) => {
+const CalendarItemWrapper = ({
+	data,
+	styleClass = '',
+	highlight = false,
+	translateOnHover = undefined,
+}: Props) => {
 	const [bgImage, setBgImage] = useState<string | null | undefined>(null);
 	const [bgImageSecondary, setBgImageSecondary] = useState<
 		string | null | undefined
@@ -26,6 +36,9 @@ const CalendarItemWrapper = ({ data, styleClass = '' }: Props) => {
 	};
 
 	useEffect(() => {
+		setShowModal(false);
+		setBgImage(null);
+		setBgImageSecondary(null);
 		if (data.game_releases.length) {
 			setBgImage(
 				data.game_releases.find((item) => item.background_image)
@@ -48,53 +61,65 @@ const CalendarItemWrapper = ({ data, styleClass = '' }: Props) => {
 			<>
 				<li
 					key={data.date}
-					className={`flex gap-[1px] h-28 shadow shadow-gray-500 bg-purple-800 border-2 border-purple-700 rounded-md relative bg-no-repeat bg-[size:100%_100%] transition-all 
+					className={` 
 					${data.isCurrentMonth ? '' : 'opacity-50 hover:opacity-100'}
-					${data.game_releases.length ? 'cursor-pointer hover:z-10 hover:scale-150' : ''}
+					${data.game_releases.length ? 'cursor-pointer hover:z-10' : ''}
+					${data.game_releases.length && translateOnHover ? translateOnHover : ''}
+					${
+						data.game_releases.length && !translateOnHover
+							? 'transition-all hover:scale-150'
+							: ''
+					}
 					${styleClass}`}
 					onClick={data.game_releases.length ? openModal : undefined}
 				>
-					{data.game_releases.length > 1 &&
-					styleClass.includes('col-span-8') ? (
-						<>
+					<div
+						className={`flex gap-[1px] h-28 shadow shadow-gray-500 bg-purple-800 border-2 border-purple-700 rounded-md relative bg-no-repeat bg-[size:100%_100%] transition-all select-none
+						${highlight ? 'shadow-lg border-white' : ''}
+						${translateOnHover ? 'translated-child' : ''}`}
+					>
+						{data.game_releases.length > 1 &&
+						styleClass.includes('col-span-8') ? (
+							<>
+								<Image
+									src={bgImage || bgEmpty}
+									alt='Game cover 1'
+									width={400}
+									height={225}
+									className={`w-full h-full rounded-tl rounded-bl
+								${!bgImage ? 'bg-empty' : ''}`}
+								/>
+								<Image
+									src={bgImageSecondary || bgEmpty}
+									alt='Game cover 2'
+									width={400}
+									height={225}
+									className={`w-full h-full rounded-tr rounded-br
+								${!bgImageSecondary ? 'bg-empty' : ''}`}
+								/>
+							</>
+						) : (
 							<Image
 								src={bgImage || bgEmpty}
-								alt='Game cover 1'
+								alt='Game cover'
 								width={400}
 								height={225}
-								className={`w-full h-full rounded-tl rounded-bl
-								${!bgImage ? 'bg-empty' : ''}`}
-							/>
-							<Image
-								src={bgImageSecondary || bgEmpty}
-								alt='Game cover 2'
-								width={400}
-								height={225}
-								className={`w-full h-full rounded-tr rounded-br
-								${!bgImageSecondary ? 'bg-empty' : ''}`}
-							/>
-						</>
-					) : (
-						<Image
-							src={bgImage || bgEmpty}
-							alt='Game cover'
-							width={400}
-							height={225}
-							className={`w-full h-full rounded
+								className={`w-full h-full rounded
 							${!bgImage ? 'bg-empty' : ''}`}
-						/>
-					)}
-					<span
-						className={`absolute top-0 left-0 font-bold text-2xl px-1 rounded-tl rounded-br leading-[1.8rem]
+							/>
+						)}
+						<span
+							className={`absolute top-0 left-0 font-bold text-2xl px-1 rounded-tl rounded-br leading-[1.8rem]
 						${data.isCurrentDay ? 'text-yellow-300 bg-blue-700' : 'bg-purple-700'}`}
-					>
-						{data.dayOfMonth}
-					</span>
-					{data.game_releases.length > 0 && (
-						<span className='absolute bottom-0 right-0 bg-green-700 font-bold px-1 text-sm rounded-tl rounded-br'>
-							{data.game_releases.length}
+						>
+							{data.dayOfMonth}
 						</span>
-					)}
+						{data.game_releases.length > 0 && (
+							<span className='absolute bottom-0 right-0 bg-green-700 font-bold px-1 text-sm rounded-tl rounded-br'>
+								{data.game_releases.length}
+							</span>
+						)}
+					</div>
 				</li>
 				{showModal && data.game_releases.length && (
 					<Modal closeModal={closeModal}>
