d14139c3f901659cb8dee8e33fa9adbb907f77d6
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-13 22:43:27 +0100

    Updating styling for mobile first design.

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -6,7 +6,7 @@ interface Props {
 const Modal = ({ children, closeModal }: Props) => {
 	return (
 		<div className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-10'>
-			<div className='flex justify-center items-center w-4/5 h-4/5'>
+			<div className='flex justify-center items-center w-[90%] h-[90%]'>
 				<div
 					className='absolute top-0 left-0 right-0 bottom-0 bg-black/75'
 					onClick={closeModal}
@@ -14,9 +14,9 @@ const Modal = ({ children, closeModal }: Props) => {
 				<div className='w-full h-full z-30 border-2 rounded-tl-lg rounded-bl-lg rounded-br-lg overflow-auto shadow-md shadow-gray-500 bg-gray-800 border-purple-800'>
 					<div className='flex flex-col gap-1'>{children}</div>
 				</div>
-				<div className='self-start w-0 z-20 mt-[4px] cursor-pointer'>
+				<div className='self-start w-0 z-20 mt-[1px] cursor-pointer'>
 					<span
-						className='border-2 p-1 pb-1.5 border-l-0 rounded-tr-lg rounded-br-lg text-red-500 text-2xl shadow shadow-gray-500 modal-close-btn bg-gray-800 border-purple-800'
+						className='border-2 pt-0 p-1 border-l-0 rounded-tr-lg rounded-br-lg text-red-500 text-2xl shadow shadow-gray-500 modal-close-btn bg-gray-800 border-purple-800'
 						onClick={closeModal}
 					/>
 				</div>
