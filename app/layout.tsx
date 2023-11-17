3279dd402c79d80b77ffb62f03b68da42bbdb7bb
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-17 20:23:06 +0100

    Adding transition property to body element

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
+import Provider from './hooks/Provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={`transition-all ${inter.className}`}>
+				<Provider>{children}</Provider>
+			</body>
 		</html>
 	);
 };
