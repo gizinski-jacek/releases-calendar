4fb8a6b7d07c2913488a0d82edb7e80f24273291
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-17 23:15:25 +0100

    Replacing placeholder text with slider

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -4,17 +4,18 @@ import { useTheme } from 'next-themes';
 
 const ThemeSwitcher = () => {
 	const { systemTheme, theme, setTheme } = useTheme();
+	const currentTheme = theme === 'system' ? systemTheme : theme;
 
 	return (
-		<button
-			onClick={() =>
-				(theme === 'system' ? systemTheme : theme) == 'dark'
-					? setTheme('light')
-					: setTheme('dark')
-			}
+		<div
+			className='self-center transition-all rounded cursor-pointer m-1 border-2 border-secondary rounded-xl bg-blue transition-all w-12'
+			onClick={() => setTheme(currentTheme === 'dark' ? 'light' : 'dark')}
 		>
-			{(theme === 'system' ? systemTheme : theme) == 'dark' ? 'Light' : 'Dark'}
-		</button>
+			<div
+				className={`w-3 h-3 m-[0.2rem] rounded-lg bg-gold transition-all
+				${theme === 'dark' ? '' : 'translate-x-6'}`}
+			/>
+		</div>
 	);
 };
 
