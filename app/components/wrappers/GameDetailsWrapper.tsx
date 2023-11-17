dd67fc52b5332f937133b667855dfbc57fb3a431
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-17 20:27:08 +0100

    Replacing style colors with custom ones.

--- app/components/wrappers/GameDetailsWrapper.tsx
+++ app/components/wrappers/GameDetailsWrapper.tsx
@@ -7,14 +7,14 @@ interface Props {
 
 const GameDetailsWrapper = ({ game }: Props) => {
 	return (
-		<div className='flex gap-4 bg-gray-800'>
+		<div className='flex gap-4 bg-gray'>
 			<div className='min-w-[30%] md:min-w-[40%] lg:min-w-[50%] flex-1 shrink-0 grow p-1 md:p-2'>
 				<div className='mb-1 md:mb-2 lg:mb-4 text-lg md:text-xl lg:text-2xl font-bold'>
 					{game.name}
 				</div>
 				<div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 md:gap-x-8 lg:gap-x-12 gap-y-2 md:gap-y-4'>
 					<div>
-						<div className='font-bold text-sm md:text-base lg:text-lg text-gray-400 italic'>
+						<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
 							Release Date
 						</div>
 						<div className='text-sm md:text-base lg:text-lg'>
@@ -23,7 +23,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					</div>
 					{game.metacritic && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-gray-400 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
 								Metacritic Score
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -33,7 +33,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.esrb_rating && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-gray-400 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
 								ESRB Rating
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -43,7 +43,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.genres?.length && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-gray-400 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
 								Genres
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -53,7 +53,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.platforms?.length && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-gray-400 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
 								Platforms
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -68,7 +68,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.stores?.length && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-gray-400 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
 								Stores
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
