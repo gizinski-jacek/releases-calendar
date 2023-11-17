2a48e574f6a7183c0ce2713daeabbdc238fe3619
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-17 20:26:38 +0100

    Updating styling

--- app/components/ModalImage.tsx
+++ app/components/ModalImage.tsx
@@ -9,8 +9,8 @@ const ModalImage = ({ children, closeModal }: Props) => {
 			className='w-screen h-screen absolute top-0 left-0 z-50'
 			onClick={closeModal}
 		>
-			<div className='absolute top-0 left-0 right-0 bottom-0 bg-black' />
-			<div className='absolute top-[50%] translate-y-[-50%] left-0  relative z-20'>
+			<div className='absolute top-0 left-0 right-0 bottom-0 bg-black/75' />
+			<div className='absolute top-[50%] translate-y-[-50%] left-0 relative z-20'>
 				{children}
 			</div>
 		</div>
