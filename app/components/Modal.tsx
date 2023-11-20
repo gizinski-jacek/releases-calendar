04fe8f1e26b7ba1a9475244c1e3daa4483d6b138
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-20 19:04:21 +0100

    Fixing close button placement.
    Updating colors vars.

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -11,15 +11,15 @@ const Modal = ({ children, closeModal }: Props) => {
 					className='absolute top-0 left-0 right-0 bottom-0 bg-black/75'
 					onClick={closeModal}
 				/>
-				<div className='w-full h-full z-30 border-2 border-secondary rounded-tl-lg rounded-bl-lg rounded-br-lg overflow-auto shadow-md shadow-primary bg-primary'>
-					<div className='flex flex-col gap-1 bg-secondary'>{children}</div>
+				<div className='w-full max-h-full z-30 border-2 border-custom-secondary rounded-tl-lg rounded-bl-lg rounded-br-lg overflow-auto shadow-md shadow-custom-primary bg-custom-primary'>
+					<div className='flex flex-col gap-1 bg-custom-secondary'>
+						{children}
+					</div>
 				</div>
-				<div className='self-start w-0 z-20 cursor-pointer transition-all'>
-					<span
-						className='pt-0 p-1 border-2 border-l-0 border-secondary rounded-tr-lg rounded-br-lg text-red-500 text-2xl shadow shadow-gray-500 modal-close-btn'
-						onClick={closeModal}
-					/>
-				</div>
+				<div
+					className='transition-all self-start z-40 cursor-pointer bg-custom-gray hover:bg-custom-primary px-1 border-2 border-l-0 border-custom-secondary rounded-tr-lg rounded-br-lg text-custom-red/75 hover:text-custom-red active:text-custom-red text-2xl shadow shadow-custom-gray-500 modal-close-btn'
+					onClick={closeModal}
+				/>
 			</div>
 		</div>
 	);
