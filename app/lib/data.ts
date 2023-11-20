34a3334a0eae3b0c0f71d60d6b3ffa65b842647b
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-20 19:51:53 +0100

    Adding links to various stores pages.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,1 +1,11 @@
-export const filterTags = ['sexual-content', 'nsfw'];
+export const filterTags: string[] = ['sexual-content', 'nsfw'];
+
+export const storeLinks: { [key: string]: string } = {
+	steam: 'https://store.steampowered.com/',
+	gog: 'https://www.gog.com/',
+	'microsoft-store': 'https://www.microsoft.com/en-aw/store/games/',
+	playstation: 'https://store.playstation.com/en-us/pages/latest',
+	'epic-games': 'https://www.epicgames.com/site/en-US/home',
+	xbox: 'https://www.xbox.com/en-GB/games',
+	nintendo: 'https://www.nintendo.com/us/store/',
+};
