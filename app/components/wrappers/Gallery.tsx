8802cb34821237a68f35bba26ab95a40c68be0e6
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-20 18:40:44 +0100

    Changing styling of active dot control
    Replacing block/hidden properties on hidden images with width property
    to preload images.

--- app/components/wrappers/Gallery.tsx
+++ app/components/wrappers/Gallery.tsx
@@ -43,7 +43,7 @@ const Gallery = ({ gallery }: Props) => {
 		<>
 			<div className='w-full h-auto max-w-[600px] relative select-none'>
 				<div
-					className='absolute top-[50%] translate-y-[-50%] left-0 border-[1px] border-gray-600 hover:border-gray-400 active:border-white text-gray-500 hover:text-white active:text-white text-sm md:text-base lg:text-lg p-2 md:p-3 lg:p-4 box-border cursor-pointer select-none transition-all bg-black/50 hover:bg-black/75 active:bg-black/90 text-white rounded-tr-md rounded-br-md'
+					className='absolute top-[50%] translate-y-[-50%] left-0 z-10 border-[1px] border-custom-gray-600 hover:border-custom-gray-400 active:border-white text-custom-gray-500 hover:text-white active:text-white text-sm md:text-base lg:text-lg p-2 md:p-3 lg:p-4 box-border cursor-pointer select-none transition-all bg-black/50 hover:bg-black/75 active:bg-black/90 text-white rounded-tr-md rounded-br-md'
 					onClick={prevSlide}
 				>
 					&#10094;
@@ -52,7 +52,7 @@ const Gallery = ({ gallery }: Props) => {
 					return (
 						<div
 							key={item.id}
-							className={`${index === slideIndex ? 'block' : 'hidden'}`}
+							className={`${index === slideIndex ? 'w-unset' : 'w-0'}`}
 						>
 							<Image
 								src={item.image}
@@ -66,7 +66,7 @@ const Gallery = ({ gallery }: Props) => {
 					);
 				})}
 				<div
-					className='absolute top-[50%] translate-y-[-50%] right-0 border-[1px] border-gray-600 hover:border-gray-400 active:border-white text-gray-500 hover:text-white active:text-white text-sm md:text-base lg:text-lg p-2 md:p-3 lg:p-4 box-border cursor-pointer select-none transition-all bg-black/50 hover:bg-black/75 active:bg-black/90 text-white rounded-tl-md rounded-bl-md'
+					className='absolute top-[50%] translate-y-[-50%] right-0 z-10 border-[1px] border-custom-gray-600 hover:border-custom-gray-400 active:border-white text-custom-gray-500 hover:text-white active:text-white text-sm md:text-base lg:text-lg p-2 md:p-3 lg:p-4 box-border cursor-pointer select-none transition-all bg-black/50 hover:bg-black/75 active:bg-black/90 text-white rounded-tl-md rounded-bl-md'
 					onClick={nextSlide}
 				>
 					&#10095;
@@ -74,7 +74,7 @@ const Gallery = ({ gallery }: Props) => {
 			</div>
 			<div className='flex justify-center items-center gap-1 md:gap-2 lg:gap-3 p-1 pb-4'>
 				<div
-					className='text-lg md:text-xl lg:text-2xl box-border cursor-pointer select-none px-1 transition-all font-bold text-secondary/60 hover:text-secondary/80 active:text-secondary'
+					className='text-lg md:text-xl lg:text-2xl box-border cursor-pointer select-none px-1 transition-all font-bold text-custom-secondary/60 hover:text-custom-secondary/80 active:text-custom-secondary'
 					onClick={prevSlide}
 				>
 					&#10094;
@@ -84,8 +84,10 @@ const Gallery = ({ gallery }: Props) => {
 						return (
 							<span
 								key={item.id}
-								className={`inline transition-all h-3 border-[2px] border-secondary/60 w-3 md:h-4 md:w-4 bg-secondary/60 hover:bg-secondary/80 active:bg-secondary rounded-lg cursor-pointer ${
-									index === slideIndex ? '!bg-gold !border-secondary' : ''
+								className={`inline transition-all h-3 border-[2px] border-custom-secondary/60 w-3 md:h-4 md:w-4 bg-custom-secondary/60 hover:bg-custom-secondary/80 active:bg-custom-secondary rounded-lg cursor-pointer ${
+									index === slideIndex
+										? '!bg-custom-secondary !border-custom-secondary'
+										: ''
 								}`}
 								onClick={() => changeSlideWithDots(index)}
 							/>
@@ -93,7 +95,7 @@ const Gallery = ({ gallery }: Props) => {
 					})}
 				</div>
 				<div
-					className='text-lg md:text-xl lg:text-2xl box-border cursor-pointer select-none px-1 transition-all font-bold text-secondary/60 hover:text-secondary/80 active:text-secondary'
+					className='text-lg md:text-xl lg:text-2xl box-border cursor-pointer select-none px-1 transition-all font-bold text-custom-secondary/60 hover:text-custom-secondary/80 active:text-custom-secondary'
 					onClick={nextSlide}
 				>
 					&#10095;
