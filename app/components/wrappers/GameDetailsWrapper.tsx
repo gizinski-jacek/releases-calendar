915b7a985847b094f4859f0d78784aaa6746fa29
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-20 19:51:38 +0100

    Updating colors vars.
    Adding link to store page.

--- app/components/wrappers/GameDetailsWrapper.tsx
+++ app/components/wrappers/GameDetailsWrapper.tsx
@@ -1,5 +1,6 @@
+import { storeLinks } from '@/app/lib/data';
 import { GameData } from '../../lib/types';
-import Gallery from './Gallery';
+import Gallery from '../Gallery';
 
 interface Props {
 	game: GameData;
@@ -7,14 +8,14 @@ interface Props {
 
 const GameDetailsWrapper = ({ game }: Props) => {
 	return (
-		<div className='flex gap-4 bg-gray'>
+		<div className='flex gap-4 bg-custom-gray'>
 			<div className='min-w-[30%] md:min-w-[40%] lg:min-w-[50%] flex-1 shrink-0 grow p-1 md:p-2'>
 				<div className='mb-1 md:mb-2 lg:mb-4 text-lg md:text-xl lg:text-2xl font-bold'>
 					{game.name}
 				</div>
 				<div className='grid grid-cols-1 md:grid-cols-2 gap-x-4 md:gap-x-8 lg:gap-x-12 gap-y-2 md:gap-y-4'>
 					<div>
-						<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
+						<div className='font-bold text-sm md:text-base lg:text-lg text-custom-secondary/50 italic'>
 							Release Date
 						</div>
 						<div className='text-sm md:text-base lg:text-lg'>
@@ -23,7 +24,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					</div>
 					{game.metacritic && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-custom-secondary/50 italic'>
 								Metacritic Score
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -33,7 +34,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.esrb_rating && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-custom-secondary/50 italic'>
 								ESRB Rating
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -43,7 +44,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.genres?.length && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-custom-secondary/50 italic'>
 								Genres
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -53,7 +54,7 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.platforms?.length && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-custom-secondary/50 italic'>
 								Platforms
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
@@ -68,14 +69,26 @@ const GameDetailsWrapper = ({ game }: Props) => {
 					)}
 					{game.stores?.length && (
 						<div>
-							<div className='font-bold text-sm md:text-base lg:text-lg text-secondary/75 italic'>
+							<div className='font-bold text-sm md:text-base lg:text-lg text-custom-secondary/50 italic'>
 								Stores
 							</div>
 							<div className='text-sm md:text-base lg:text-lg'>
 								{game.stores
 									.sort((a, b) => a.store.name.localeCompare(b.store.name))
-									.map((item) => item.store.name)
-									.join(', ')}
+									.map((item, i, arr) => (
+										<>
+											<a
+												key={item.store.slug}
+												href={storeLinks[item.store.slug]}
+												target='_blank'
+												rel='noreferrer'
+												className='underline'
+											>
+												{item.store.name}
+											</a>
+											{i !== arr.length - 1 && ', '}
+										</>
+									))}
 							</div>
 						</div>
 					)}
