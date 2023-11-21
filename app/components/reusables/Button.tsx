588d4ae1fc7b716c37266ba213d5e28f82cc76e6
Jacek Gizinski <jacektrg+github@gmail.com>
2023-11-21 22:25:02 +0100

    Updating props, styling and types.

--- app/components/reusables/Button.tsx
+++ app/components/reusables/Button.tsx
@@ -1,23 +1,23 @@
 interface Props {
-	data: any;
+	children?: React.ReactNode;
 	styleClass?: string;
 	styleInline?: { [key: string]: string };
 	cta: () => any;
 }
 
 const Button = ({
-	data,
+	children,
 	styleClass = '',
 	styleInline = undefined,
 	cta,
 }: Props) => {
 	return (
 		<div
-			className={`col-span-4 transition-all uppercase text-sm lg:text-base text-center font-semibold text-custom-secondary border-2 border-custom-secondary/80 hover:border-custom-secondary/90 active:border-custom-secondary bg-custom-blue/75 hover:bg-custom-blue/90 active:bg-custom-blue hover:shadow active:shadow-md shadow-custom-secondary/75 hover:shadow-custom-secondary/90 active:shadow-custom-secondary/90 rounded cursor-pointer select-none ${styleClass}`}
+			className={`col-span-4 transition-all uppercase text-sm lg:text-base text-center font-semibold text-custom-secondary border-2 border-custom-secondary/80 hover:border-custom-secondary/90 active:border-custom-red active:border-custom-secondary bg-custom-blue/75 hover:bg-custom-blue/90 active:bg-custom-blue hover:shadow shadow-custom-secondary/75 hover:shadow-custom-secondary/90 active:shadow-custom-secondary/90 rounded cursor-pointer select-none ${styleClass}`}
 			style={styleInline}
 			onClick={cta}
 		>
-			{data}
+			{children||'Button'}
 		</div>
 	);
 };
