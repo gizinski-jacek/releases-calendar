51ade7c053942c89b350c0cc5e353bef84f67884
Jacek Gizinski <jacektrg+github@gmail.com>
2024-05-07 21:44:48 +0200

    Changing z-index value

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import style from '../styles/LoadingSpinner.module.scss';
 
 const LoadingSpinner = () => {
 	return (
-		<div className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-[75]'>
+		<div className='absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-[100]'>
 			<div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50' />
 			<div className={style.spinner}>
 				<div></div>
